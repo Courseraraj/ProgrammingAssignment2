6e78ca3f551134280bde1ca3c5069d72681c8b0e
RajKumarl <lingampellyrajkumar@gmail.com>
2015-04-25 20:37:16 +0530

    Revert "Assignment2"
    This reverts commit 04b70274dd6e0622b7975948f1053300fc73c3ae.

--- cachematrix.R
+++ cachematrix.R
@@ -1,57 +1,15 @@
-## makeCacheMatrix creates a special matrix object, and then cacheSolve 
-## calculates the inverse of the matrix.
-## If the matrix inverse has already been calculated, it will instead 
-## find it in the cache and return it, and not calculate it again.
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  ## @x: a square invertible matrix
-  ## return: a list containing functions to
-  ##              1. set the matrix
-  ##              2. get the matrix
-  ##              3. set the inverse
-  ##              4. get the inverse
-  ##         this list is used as the input to cacheSolve()
-  
-  inv = NULL
-  set = function(y) {
-    
-    x <<- y
-    inv <<- NULL
-  }
-  get = function() x
-  setinv = function(inverse) inv <<- inverse 
-  getinv = function() inv
-  list(set=set, get=get, setinv=setinv, getinv=getinv)
-}
+
 }
 
 
-## The function cacheSolve returns the inverse of a matrix A created with
-## the makeCacheMatrix function.
-## If the cached inverse is available, cacheSolve retrieves it, while if
-## not, it computes, caches, and returns it.
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-  ## @x: output of makeCacheMatrix()
-  ## return: inverse of the original matrix input to makeCacheMatrix()
-  
-  inv = x$getinv()
-  
-  # if the inverse has already been calculated
-  if (!is.null(inv)){
-    # get it from the cache and skips the computation. 
-    message("getting cached data")
-    return(inv)
-  }
-  
-  # otherwise, calculates the inverse 
-  mat.data = x$get()
-  inv = solve(mat.data, ...)
-  
-  # sets the value of the inverse in the cache via the setinv function.
-  x$setinv(inv)
-  
-  return(inv)
-}
-
+        ## Return a matrix that is the inverse of 'x'
 }
